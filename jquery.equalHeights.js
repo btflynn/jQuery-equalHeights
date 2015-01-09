1fbfa4d8b9dd6060ceb01a164d3a96417bf8a6e4
Brandon Flynn <btflynn@gmail.com>
2015-01-09 09:20:33 -0500

    cleaning up JS a bit

--- jquery.equalHeights.js
+++ jquery.equalHeights.js
@@ -22,20 +22,50 @@
           tallest_child_height = child_height;
         }
       }
-      children.height(tallest_child_height);
+      children.height(tallest_child_height); 
     };
 
     // Run initializer
     base.init();
-  };
+  }
 
   $.fn.equalHeights = function(){
     return this.each(function(){
       (new $.equalHeights(this));
     });
   };
-
+    
 })(jQuery, window, document);
 
 // Basic Usage -> Call equalHeights on any container and enjoy the resulting height sameness on all its children with class="equal-heights"...
 //$('#container1').equalHeights();
+
+
+
+// DEMO CODE //
+var evaluateHeights = function(){
+  var $height_reports = $('.height-report');
+  
+  for(var i=0; i < $height_reports.length; i++) {
+    var $this = $height_reports.eq(i),
+        $parent = $this.closest('.equal-heights'),
+        height = $parent.height();
+    $this.empty().append("I am " + height + "px tall.");
+  }
+}
+
+$(document).ready(function(){
+  evaluateHeights();
+});
+
+// Triggered Scenario for Demo
+$('.trigger').on('click', function(e){
+  var $container = $('#container1');
+  $container.equalHeights().trigger('equalized');
+});
+
+$('body').on('equalized', function(){
+  var $columns = $('.equal-heights');
+  evaluateHeights();
+  $columns.addClass('equalized');
+});
